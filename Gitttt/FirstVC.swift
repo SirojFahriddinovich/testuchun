24fa52ac3e3857cc355e845f9f83ee8f3a56ebb1
SirojFahriddinovich <sirojiddinqutbiddinov2@gmail.com>
2022-05-19 23:07:49 +0500

    First

---
+++ Gitttt/FirstVC.swift
@@ -0,0 +1,17 @@
+//
+//  FirstVC.swift
+//  Gitttt
+//
+//  Created by Macbook Air on 19/05/22.
+//
+
+import UIKit
+
+class FirstVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
