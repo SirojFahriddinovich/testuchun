6ae1b29071abbdcd0962cd2750085d8a836e2c7f
SirojFahriddinovich <sirojiddinqutbiddinov2@gmail.com>
2022-05-19 23:09:15 +0500

    salommm,,

---
+++ Gitttt/SecondVC.swift
@@ -0,0 +1,18 @@
+//
+//  SecondVC.swift
+//  Gitttt
+//
+//  Created by Macbook Air on 19/05/22.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemRed
+    }
+    
+}
